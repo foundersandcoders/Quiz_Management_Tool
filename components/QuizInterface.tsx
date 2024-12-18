7aa100d82c3f313ee66671b3b86aa3bf562b65af
Shaughn <Shaughn@MacBook-Air.local>
2024-12-18 17:56:50 +0000

    working submit answers

--- components/QuizInterface.tsx
+++ components/QuizInterface.tsx
@@ -1,50 +1,66 @@
 'use client'
-import {  useState } from 'react';
+import uploadAnswers from '@/utils/supabase/uploadAnswers';
+import {  useState, useMemo } from 'react';
 
 
 export default function QuizInterface(quizData){
     const [questionAnswers, setQuestionAnswers] = useState([]);
+    const [shuffledQuestions, setShuffledQuestions] = useState([]);
 
-function shuffleArray (array:string[]) {
+    useMemo(() => {
+        const shuffled = quizData.quizData.questions.map((question) => {
+            const options = [question.question_answer, ...question.question_false_answers];
+            return {
+                ...question,
+                shuffledOptions: shuffleArray(options),
+            };
+        });
+        setShuffledQuestions(shuffled);
+    }, []); 
+
+    function shuffleArray (array:string[]) {
 
 
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+    function choiceSelectHandler(questionId, answer){
+        const filteredAnswers = questionAnswers.filter(answer=> (answer.questionId !== questionId))
+        filteredAnswers.push({questionId: questionId, response: answer})
+    setQuestionAnswers(filteredAnswers)
+    console.log(filteredAnswers)
     }
-    return array;
-};
-function choiceSelectHandler(questionID, answer){
-    const filteredAnswers = questionAnswers.filter(answer=> (answer.questionNumber !== questionId))
-    filteredAnswers.push({questionNumber: questionID, response: answer})
-setQuestionAnswers(filteredAnswers)
-console.log(filteredAnswers)
-}
-const letterArray= ['a) ', 'b) ','c) ','d) ','e) ' ]
+
+
+    function submitHandler(questionAnswers, quizId){
+
+uploadAnswers(questionAnswers,  quizId)
+    }
+
+    const letterArray= ['a) ', 'b) ','c) ','d) ','e) ' ]
     return(
         <div>
     <h1>{quizData.quizData.quiz_name}</h1>
     <h2>assigned {quizData.quizData.opens_at}</h2>
     <h2>due date {quizData.quizData.closes_at}</h2>
     {/* format time */}
-    {quizData.quizData.questions.map((question, index)=>{
-        const options = [question.question_answer, ...question.question_false_answers];
-        // Shuffle the options
-        const shuffledOptions = shuffleArray(options);
-
-        return(
-        <div>
-        <p>{index +1}. {question.question_text} </p>
-        <ul>
-                            {shuffledOptions.map((option, optionIndex) => (
-                                <li key={optionIndex} onClick={()=>choiceSelectHandler(question.id, option)}>{letterArray[optionIndex] + option}</li>
-                            ))}
-                        </ul>
+    {shuffledQuestions.map((question, index) => (
+        <div key={question.id}>
+            <p>{index + 1}. {question.question_text} </p>
+            <ul>
+                {question.shuffledOptions.map((option, optionIndex) => (
+                    <li key={optionIndex} onClick={() => choiceSelectHandler(question.id, option)}>
+                        {letterArray[optionIndex] + option}
+                    </li>
+                ))}
+            </ul>
         </div>
-        )
-    })}
+    ))}
     
-    <button>Submit Answers</button>
+    <button onClick={()=> submitHandler(questionAnswers,quizData.quizData.id)}>Submit Answers</button>
         </div>
     )
 }

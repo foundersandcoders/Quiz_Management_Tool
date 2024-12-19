23bf0edda8fd9ba56a7a9cbe70344788e767b20e
Shaughn <Shaughn@MacBook-Air.local>
2024-12-19 16:24:00 +0000

    quiz interface highlights and redirects quizzes page sorts

--- app/quizzes/page.tsx
+++ app/quizzes/page.tsx
@@ -4,30 +4,52 @@ const Quizzes = async () => {
   const supabase = await createClient();
 
   const { data: UserInformation } = await supabase.auth.getUser();
-  const { data: userCohort } = await supabase
+  const { data: userData } = await supabase
     .from('learners')
-    .select('cohort_number')
+    .select('id,cohort_number')
     .eq('email', UserInformation.user?.email);
   const { data: quizzes } = await supabase
     .from('quizzes')
     .select(
       `
     *,
-    quiz_questions(*)
+    quiz_question_learner_answers(learner_id)
     
     `
     )
-    .eq('cohort_number', userCohort[0].cohort_number);
-  console.log('userCohort', userCohort[0].cohort_number);
-  console.log('quizzes', quizzes);
+    .eq('cohort_number', userData[0].cohort_number);
+ 
   if (!UserInformation) {
     return <div>User not found</div>;
   }
+  // console.log('quizquestionLearneranswers quiz 3', quizzes[2].quiz_question_learner_answers)
+  // console.log('userData', userData)
+  // console.log('filtered results',quizzes?.filter(quiz =>quiz.quiz_question_learner_answers.some((id)=>id.learner_id == userData.id)))
   return (
     <div>
-      <p>{userCohort[0].cohort_number}</p>
+      <p>{userData[0].cohort_number}</p>
+<h1>Current Quizzes</h1>
+{quizzes.filter(quiz => new Date(quiz.closes_at) > new Date()).map((quiz, index) => {
 
-      {quizzes.map((quiz, index) => {
+        return (
+          <div key={index}>
+            <h1>{quiz.quiz_name}</h1>
+            <p>Due date {quiz.closes_at}</p>
+          </div>
+        );
+      })}
+<h1>Overdue Quizzes</h1>
+{quizzes.filter(quiz => new Date(quiz.closes_at) <= new Date() && quiz.quiz_question_learner_answers.some((id)=>id.learner_id != userData[0].id)).map((quiz, index) => {
+        return (
+          <div key={index}>
+            <h1>{quiz.quiz_name}</h1>
+            <p>Due date {quiz.closes_at}</p>
+          </div>
+        );
+      })}
+<h1>Completed Quizzes</h1>
+
+      {quizzes?.filter(quiz =>quiz.quiz_question_learner_answers.some((id)=>id.learner_id == userData[0].id)).map((quiz, index) => {
         return (
           <div key={index}>
             <h1>{quiz.quiz_name}</h1>

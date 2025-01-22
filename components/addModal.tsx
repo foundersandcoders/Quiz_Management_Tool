7f6746d8f63f89ff3a0184a044fb3a8ea24cd9ee
Shaughn <Shaughn@MacBook-Air.local>
2025-01-22 17:58:31 +0000

    modal in progress

--- components/addModal.tsx
+++ components/addModal.tsx
@@ -1,7 +1,90 @@
-import React, { useState } from 'react';
+export default function AddModal({targetTable, setIsOpen, addType = ''}: {targetTable: string}) {
+    const switchCaseDeterminObject = {targetTable: targetTable, addType: addType}
+   
+    type FieldConfig = {
+        name: string;
+        label: string;
+        type: 'short' | 'long';
+        defaultValue?: string | number;
+        required?: boolean;
+        validation?: (value: any) => boolean | string;
+      }
 
-export default function AddModal({targetTable}):({targetTable : string []}){
-    const [isOpen, setIsOpen] = useState(false);
+    function setDefaultFormData(switchCaseDeterminObject){
+       let defaultFormData:FieldConfig[] = []
+        switch(switchCaseDeterminObject){
+            case {targetTable: 'questions', addType:'add new question'}:{
+                defaultFormData  = [
+                    {
+                        name: "Question Text",
+                        label: "Question Text",
+                        type: 'long', 
+                        required: true
+                      },
+                      {
+                        name: "Correct Answer",
+                        label: "Correct Answer",
+                        type: 'short', 
+                        required: true
+                      },
+                      {
+                        name: "Wrong Answer",
+                        label: "Wrong Answer",
+                        type: 'short', 
+                        required: true
+                      },
+                      {
+                        name: "Wrong Answer",
+                        label: "Wrong Answer",
+                        type: 'short', 
+                        required: true
+                      },
+                      {
+                        name: "Wrong Answer",
+                        label: "Wrong Answer",
+                        type: 'short', 
+                        required: true
+                      }
+    
+]
 
+}
+
+}
+
+   
+    break; 
+
+
+   }
+   
+
+
+
+
+
+   
+    function handleSubmit (e) {
+        e.preventDefault();
+        switch(switchCaseDeterminObject){
+            case {targetTable: 'questions', addType:'add new question'}:
+
+
+   
+    break;
+        }
+    }
+
+return (
+<div>
+<form  onSubmit={handleSubmit}>
+
+
+<button type='submit'>submit</button>
+</form>
+
+</div>
+
+)
 
 }

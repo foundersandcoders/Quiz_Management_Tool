b95df5bd969127b5b5c6e3da0b0539ac8733e215
ShaughnAnderson94 <andersji94@gmail.com>
2025-02-07 17:31:37 +0000

    fixed errors with add quiz and blocked adding to active

--- components/QuizInterface.tsx
+++ components/QuizInterface.tsx
@@ -96,6 +96,13 @@ router.push('/quizzes');
     }
 
     const letterArray= ['a) ', 'b) ','c) ','d) ','e) ' ]
+
+    const canEditQuiz = () => {
+        const quizStartDate = new Date(quizData.opens_at);
+        const now = new Date();
+        return now < quizStartDate;
+    };
+
     return(
         <div className="max-w-4xl mx-auto p-4">
             <div className="border-b border-gray-200 mb-8">
@@ -199,10 +206,14 @@ router.push('/quizzes');
                     )}
                 </div>  
             ))}
-            {viewMode == 'admin' && <button 
-            onClick={()=>addQuestionHandler()}
-            className="button"
-            >Add Question</button>} 
+            {viewMode === 'admin' && canEditQuiz() && (
+                <button 
+                    onClick={()=>addQuestionHandler()}
+                    className="button"
+                >
+                    Add Question
+                </button>
+            )}
             {addQuestionIsOpen && <AddModal dataFunction={addQuestion}  setIsOpen={setAddQuestionIsOpen} relevantId={quizData.id} />}
             {/* add logic so this only appears if quiz has not gone live yet */}
            {viewMode == 'quiz taker' && <button 

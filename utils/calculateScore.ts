bcbef59c6983ca82351caf9743f8e44ebef3a561
Shaughn <Shaughn@MacBook-Air.local>
2025-02-17 17:07:01 +0000

    fixed last of the type errors

--- utils/calculateScore.ts
+++ utils/calculateScore.ts
@@ -4,10 +4,7 @@ import correctCheck from "./correctCheck";
 export default function calculateScores(quizData:quiz, answerData: quizResponse[], learnerId: number){
   let score = 0
     quizData.questions.forEach(question => {
-      if(correctCheck(answerData, question.id, quizData, learnerId)) {score++}
+     if(question.id && correctCheck(answerData, question.id, quizData, learnerId)) {score++}
     });
     return score
 }
-//takes the list of questions in quiz data then passes does a for each 
-//that for each question passes the question id and all relevant data
-// to correct check

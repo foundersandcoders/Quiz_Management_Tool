71f0b426c809d471bfac8c3a4894e39b233e32a5
ShaughnAnderson94 <andersji94@gmail.com>
2025-02-20 16:50:00 +0000

    test run on react form hook

---
+++ utils/supabase/editQuestion.ts
@@ -0,0 +1,6 @@
+import { FormData } from '@/types/formTypes';
+import { createClient } from '@/utils/supabase/client';
+
+export default async function editQuestion(formData:FormData, questionId:number){
+console.log(formData)
+}
